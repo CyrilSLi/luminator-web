3fabc418ccfab1d43f60f3282f628a63aa03e2a5
Cyril Li <cyrils.li@icloud.com>
2026-08-16 08:00:21 -0500

    add 512kb club badge

--- scripts/build.py
+++ scripts/build.py
@@ -8,16 +8,18 @@ with open(abs_path("../src/index.html")) as f:
     index_html = f.read()
     dev_html = index_html
 
+
+
 with open(abs_path("../src/fonts.json")) as f:
     fonts_json = f.read()
     dev_html = dev_html.replace('"%FONTS_JSON%"', fonts_json)
-    index_html = index_html.replace('"%FONTS_JSON%"', f'JSON.parse(new TextDecoder().decode(await decompressBase64("{compress_base64(fonts_json)}")))')
+    index_html = index_html.replace('"%FONTS_JSON%"', f'JSON.parse(await decompressBase64("{compress_base64(fonts_json)}", true))')
 
 with open(abs_path("../src/gif.js")) as f:
     gif_js = f.read()
     dev_html = dev_html.replace('"%GIF_JS%"', gif_js)
     index_html = index_html.replace('"%GIF_JS%"', '''const script = document.createElement("script");
-                script.textContent = new TextDecoder().decode(await decompressBase64("%GIF_JS%"));
+                script.textContent = await decompressBase64("%GIF_JS%", true);
                 document.body.appendChild(script);
                 script.remove()'''.replace("%GIF_JS%", compress_base64(gif_js)))
 
@@ -27,6 +29,13 @@ with open(abs_path("../src/gif.worker.js")) as f:
     index_html = index_html.replace('"%GIF_WORKER_JS%"', f'await decompressBase64("{compress_base64(gif_worker_js)}")')
 
 index_html = index_html.replace("// %ASYNC_HEADER%", "(async () => {").replace("// %ASYNC_FOOTER%", "})();").replace("/* async */", "async")
+
+with open(abs_path("../src/512kb-club.svg")) as f:
+    club_svg = f.read()
+    dev_html = dev_html.replace('"%512KB_CLUB%"', f"`{club_svg}`")
+    index_html = index_html.replace('"%512KB_CLUB%"', f'await decompressBase64("{compress_base64(club_svg)}", true)')
+
+
 
 with open(abs_path("../index.dev.html"), "w") as f:
     f.write(dev_html)
